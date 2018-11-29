b58f087f396d8a730ee3969114549d1cd3b08287
Alvaro Lopez Garcia <lopeza@alpha25.aulas.gsyc.urjc.es>
2018-11-29 17:50:43 +0100

    Realizada prueba final

--- P1/BlackFriday/DescuentoBlackFriday.java
+++ P1/BlackFriday/DescuentoBlackFriday.java
@@ -1,8 +1,7 @@
-package BlackFriday;
 
 public class DescuentoBlackFriday {
 	//Pasamos como parametro si estamos en el 23 de noviembre, en vez de mirar por calendar
-	
+
 	public static double PrecioFinal(double PrecioOriginal,boolean day) {
 		if (day && PrecioOriginal>0)
 		{
